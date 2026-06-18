eece550cbf5163081dc99bf153f70bdf05976636
Ramesh Swami <rameshswami4444@gmail.com>
2026-06-18 20:32:03 +0530

    first commit

--- script1.js
+++ script1.js
@@ -68,52 +68,56 @@ const notesData = [
         description: "Accounting with inventroy",
         topics: [{
                 name: "case study 2 part 1",
-                link: "https://drive.google.com/file/d/1ly8BOFm5l2ZOOYSllDGIVVqu314m7NWl/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1l69UaUUFUAu0EOyUBh6g9Vs5NueN5CE_/view?usp=sharing"
             },
             {
                 name: "case study 2 part 2",
-                link: "https://drive.google.com/file/d/1zK7Dwc-rvC24-wPBP7ea6-ouPd3rvGKW/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1mmr7doDx823sd7lNV3fcPkZtrR2qh8Fx/view?usp=sharing"
             },
             {
                 name: "case study 2 part 3",
-                link: "https://drive.google.com/file/d/1GSx4F7s_6K1zMNqwH0fc0fvYEmHJ5m_t/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1UxfNXj_K-SJ-kZtbKmdyHUMwju86RCEa/view?usp=sharing"
             },
             {
                 name: "case study 2 part 4",
-                link: "https://drive.google.com/file/d/115zrhigfVdpjSyB_P9Bq_dtw5QSQ1P9x/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1sHJ6yPvVE45ViHpmt1eVf5sv2IOAIsbm/view?usp=sharing"
             },
             {
                 name: "case study 2 part 5",
-                link: "https://drive.google.com/file/d/1pNLHnku8Dw67hu-So6fENexF_zhcnA36/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1d-omXeUjPLd7hUoy_-nNS6SnyI3fSKfF/view?usp=sharing"
             },
             {
                 name: "case study 2 part 6",
-                link: "https://drive.google.com/file/d/1uTwhdAUa6Wlu8K89UBZFoQcWY2wGXQbQ/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1t8ioyhxbDHYt_r6nGzYY5-9WLO4oBtVR/view?usp=sharing"
             },
             {
                 name: "case study 2 part 7",
-                link: "https://drive.google.com/file/d/1U440wuAvqdHZJ2D_0o4n1s5d3rAMOEOQ/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1jCkjCyl75wTnfYRxJL_ZC7_TjJj4VCCh/view?usp=sharing"
             },
             
             {
                 name: "case study 2 part 8",
-                link: "https://drive.google.com/file/d/1JudCszfGvAYLVs58DjH90-AyA-TVu4yc/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1RKi3W7ic2YVdVmceMruE2tvGihQFJAeH/view?usp=sharing"
             },
             {
                 name: "case study 2 part 9",
-                link: "https://drive.google.com/file/d/1_2vKN_bWhZbn0-BD9CHFTuWC28YBCc8T/view?usp=sharing"
+                link: "https://drive.google.com/file/d/11-92jaW3Rcft2QSfbb-dj_5Pcur7lJHs/view?usp=sharing"
             },
             {
                 name: "case study 2 part 10",
-                link: "https://drive.google.com/file/d/1rez8donOvzoj0-SfDbNPXzqgeiCJshk-/view?usp=sharing"
+                link: "https://drive.google.com/file/d/15n6fnvcHt2OX1CFWT3eCO7_HBjWgdBlZ/view?usp=sharing"
             },
             {
                 name: "case study 2 part 11",
-                link: "https://drive.google.com/file/d/1DMtVMX_pEqRj61QyJCLoW9bD926jIvoL/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1gk_EZIYf-KxTC2QtQWUUCqICDet_QYkf/view?usp=sharing   "
             },
             {
                 name: "case study 2 part 12",
-                link: "https://drive.google.com/file/d/1krpYveUA2azMKgbn3FMHEVfMTusoMy7n/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1Hjv8sKLUI4h_pE8x4uc7N09WyO8LB4oM/view?usp=sharing"
+            },
+              {
+                name: "case study 2 part 12",
+                link: "https://drive.google.com/file/d/1sGSvnza4nn1RvKN1ISmEFMaiO_OszLSE/view?usp=sharing"
             },
 
             
@@ -127,53 +131,50 @@ const notesData = [
         topics: [
            {
                 name: "case study 3 part 1",
-                link: "https://drive.google.com/file/d/1ly8BOFm5l2ZOOYSllDGIVVqu314m7NWl/view?usp=sharing"
+                link: "https://drive.google.com/file/d/185pOjewzjMoGJVv2vf6V-tDP7fgPF5OP/view?usp=sharing"
             },
             {
                 name: "case study 3 part 2",
-                link: "https://drive.google.com/file/d/1zK7Dwc-rvC24-wPBP7ea6-ouPd3rvGKW/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1KNtkreoYWgTGjo-CKt_rXBYOloInppZY/view?usp=sharing"
             },
             {
                 name: "case study 3 part 3",
-                link: "https://drive.google.com/file/d/1GSx4F7s_6K1zMNqwH0fc0fvYEmHJ5m_t/view?usp=sharing"
+                link: "https://drive.google.com/file/d/17q7CSXarIy7d5nO58F6VBNrDRW0Q5Stl/view?usp=sharing"
             },
             {
                 name: "case study 3 part 4",
-                link: "https://drive.google.com/file/d/115zrhigfVdpjSyB_P9Bq_dtw5QSQ1P9x/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1XWBIKvgpV_EamNverxOnftL--vQSKZBu/view?usp=sharing"
             },
             {
                 name: "case study 3 part 5",
-                link: "https://drive.google.com/file/d/1pNLHnku8Dw67hu-So6fENexF_zhcnA36/view?usp=sharing"
+                link: "https://drive.google.com/file/d/16RUBJUDwHiR3e6Sx0u7k7LlY0TCfjAyT/view?usp=sharing"
             },
             {
                 name: "case study 3 part 6",
-                link: "https://drive.google.com/file/d/1uTwhdAUa6Wlu8K89UBZFoQcWY2wGXQbQ/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1aYhEuAhXbHe0zosLwnzBMuTi0AG_UMRi/view?usp=sharing"
             },
             {
                 name: "case study 3 part 7",
-                link: "https://drive.google.com/file/d/1U440wuAvqdHZJ2D_0o4n1s5d3rAMOEOQ/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1cNX1udHmupCkLJKEIFfL8P5AsnY0BXHv/view?usp=sharing"
             },
             
             {
                 name: "case study 3  part 8",
-                link: "https://drive.google.com/file/d/1JudCszfGvAYLVs58DjH90-AyA-TVu4yc/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1xg-PVJoEmt8IY9n5O3BhROh5xwe4M_0c/view?usp=sharing"
             },
             {
                 name: "case study 3 part 9",
-                link: "https://drive.google.com/file/d/1_2vKN_bWhZbn0-BD9CHFTuWC28YBCc8T/view?usp=sharing"
+                link: "https://drive.google.com/file/d/12KoDyCI9u1H-PZ2QZzI82k4bk1O1COzg/view?usp=sharing"
             },
             {
                 name: "case study 3 part 10",
-                link: "https://drive.google.com/file/d/1rez8donOvzoj0-SfDbNPXzqgeiCJshk-/view?usp=sharing"
+                link: "https://drive.google.com/file/d/13gsZFpYsRT2ZAR53elM8pyZ2u7_k8Fiq/view?usp=sharing"
             },
             {
                 name: "case study 3 part 11",
-                link: "https://drive.google.com/file/d/1DMtVMX_pEqRj61QyJCLoW9bD926jIvoL/view?usp=sharing"
+                link: "https://drive.google.com/file/d/1dZuDNUYI5wnN-93bxRcxW2EmU3nlI1La/view?usp=sharinghttps://drive.google.com/file/d/1DMtVMX_pEqRj61QyJCLoW9bD926jIvoL/view?usp=sharing"
             },
-            {
-                name: "case study 3 part 12",
-                link: "https://drive.google.com/file/d/1krpYveUA2azMKgbn3FMHEVfMTusoMy7n/view?usp=sharing"
-            },
+           
            
         ]
     },
@@ -184,54 +185,43 @@ const notesData = [
         description: "Service Organization with GST",
         topics: [
            {
-                name: "case study 2 part 1",
-                link: "https://drive.google.com/file/d/1ly8BOFm5l2ZOOYSllDGIVVqu314m7NWl/view?usp=sharing"
+                name: "case study 4 part 1",
+                link: "https://drive.google.com/file/d/1iB5jwH7SwlwSyu0uxVdOGpdaQoUg2fSy/view?usp=sharing"
             },
             {
-                name: "case study 2 part 2",
-                link: "https://drive.google.com/file/d/1zK7Dwc-rvC24-wPBP7ea6-ouPd3rvGKW/view?usp=sharing"
+                name: "case study 4 part 2",
+                link: "https://drive.google.com/file/d/1mIjntJF1_rvhQz8XrM6bnYaxXO99DMLl/view?usp=sharing"
             },
             {
-                name: "case study 2 part 3",
-                link: "https://drive.google.com/file/d/1GSx4F7s_6K1zMNqwH0fc0fvYEmHJ5m_t/view?usp=sharing"
+                name: "case study 4 part 3",
+                link: "https://drive.google.com/file/d/1wo-xUMRxnxkUSEhCHlF7lT34LbncoguL/view?usp=sharing"
             },
             {
-                name: "case study 2 part 4",
-                link: "https://drive.google.com/file/d/115zrhigfVdpjSyB_P9Bq_dtw5QSQ1P9x/view?usp=sharing"
+                name: "case study 4 part 4",
+                link: "https://drive.google.com/file/d/1UuEAl5N0EgMeimq7V2B_QJf6uhuKZHPU/view?usp=sharing"
             },
             {
-                name: "case study 2 part 5",
-                link: "https://drive.google.com/file/d/1pNLHnku8Dw67hu-So6fENexF_zhcnA36/view?usp=sharing"
+                name: "case study 4 part 5",
+                link: "https://drive.google.com/file/d/1qj8uRK5O_eq4rn_TTcRAF4h6kykSWY6I/view?usp=sharing"
             },
             {
-                name: "case study 2 part 6",
-                link: "https://drive.google.com/file/d/1uTwhdAUa6Wlu8K89UBZFoQcWY2wGXQbQ/view?usp=sharing"
+                name: "case study 4 part 6",
+                link: "https://drive.google.com/file/d/1UkBcQniN-t4mrot8kH82cR9B8fI_ZAzx/view?usp=sharing"
             },
             {
-                name: "case study 2 part 7",
-                link: "https://drive.google.com/file/d/1U440wuAvqdHZJ2D_0o4n1s5d3rAMOEOQ/view?usp=sharing"
+                name: "case study 4 part 7",
+                link: "https://drive.google.com/file/d/14kEWRnbFmmEsY23iQvCG_MAOXviq47Rz/view?usp=sharing"
             },
             
             {
-                name: "case study 2 part 8",
-                link: "https://drive.google.com/file/d/1JudCszfGvAYLVs58DjH90-AyA-TVu4yc/view?usp=sharing"
+                name: "case study 4 part 8",
+                link: "https://drive.google.com/file/d/1MjWfWiOb3OdNHK1zlRdswMVSppT-CXX5/view?usp=sharing"
             },
             {
-                name: "case study 2 part 9",
-                link: "https://drive.google.com/file/d/1_2vKN_bWhZbn0-BD9CHFTuWC28YBCc8T/view?usp=sharing"
+                name: "case study 4 part 9",
+                link: "https://drive.google.com/file/d/1yrqStX_aQHpt46yjqXckLqjQLGPaaJgD/view?usp=sharing"
             },
-            {
-                name: "case study 2 part 10",
-                link: "https://drive.google.com/file/d/1rez8donOvzoj0-SfDbNPXzqgeiCJshk-/view?usp=sharing"
-            },
-            {
-                name: "case study 2 part 11",
-                link: "https://drive.google.com/file/d/1DMtVMX_pEqRj61QyJCLoW9bD926jIvoL/view?usp=sharing"
-            },
-            {
-                name: "case study 2 part 12",
-                link: "https://drive.google.com/file/d/1krpYveUA2azMKgbn3FMHEVfMTusoMy7n/view?usp=sharing"
-            },
+           
 
         ]
     },
